9869c9636d130fab707cd132ea06f485b9a6ce36
turturicarv2 <daniel.plamadeala03@gmail.com>
2025-06-30 16:50:12 +0300

    integrate docs

--- src/jokes/jokes.controller.ts
+++ src/jokes/jokes.controller.ts
@@ -1,16 +1,22 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import { JokesService } from './jokes.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('jokes')
 @Controller('jokes')
 export class JokesController {
     constructor(private readonly jokesService: JokesService) {}
 
     @Get()
+    @ApiOperation({ summary: 'Get a random dad joke' })
+    @ApiResponse({ status: 200, description: 'A dad joke' })
     getRandomJoke() {
         return this.jokesService.getRandomJoke();
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Get a dad joke by id' })
+    @ApiResponse({ status: 200, description: 'A dad joke by id' })
     getJokeById(@Param('id') id: string) {
         return this.jokesService.getJokeById(+id);
     }
